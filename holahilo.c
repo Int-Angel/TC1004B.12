a299d59fa7d4ea690f9fc14d8e109d79bd63d45a
Int-Angel <Padilla.angel88@gmail.com>
2021-10-11 13:52:24 +0000

    hola hilos

--- holahilo.c
+++ holahilo.c
@@ -1,10 +1,18 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #define NUM_THREADS 20
 
+int saldo;
+
 void *printHola(void *arg){
     printf("Hola desde un hilo\n");
+    int saldolocal = saldo;
+
+    saldolocal += 100;
+    saldo = saldolocal;
+
     pthread_exit(NULL);
 }
 
@@ -12,10 +20,17 @@ int main(){
 
     pthread_t threads[NUM_THREADS];
 
+    saldo = 0;
     for(int i=0; i<NUM_THREADS; i++){
         pthread_create(&threads[i], NULL, printHola, NULL);
     }
 
+    //sleep(1);
+
+    for(int i=0; i<NUM_THREADS; i++){
+        pthread_join(threads[i], NULL);
+    }
+    printf("saldo = %d\n", saldo);
     pthread_exit(NULL);
 }
 
